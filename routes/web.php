c1612442107240297441950f5c00c3252dfdee06
aleixtorzap <aleixtorzap@campus.monlau.com>
2024-05-06 16:33:07 +0200

    mejoras dashboard

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::redirect('/', '/login');
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
